9f3d4c45fdc7c6c2840315fef942ed2f5c3bc24e
Cezar Gonzalez Filho <cezar.gfilho@gmail.com>
2021-08-15 22:04:05 -0300

    implementação de relatório de matrículas com testes de integração

--- src/main/java/br/com/alura/school/enrollment/EnrollmentController.java
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentController.java
@@ -1,17 +1,20 @@
 package br.com.alura.school.enrollment;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.school.course.CourseRepository;
+import br.com.alura.school.user.UserReportResponse;
 
 @RestController
 class EnrollmentController {
@@ -35,7 +38,15 @@ class EnrollmentController {
 			enrollmentRepository.save(newEnrollmentRequest.toEntity(courseCode));
 			return ResponseEntity.status(HttpStatus.CREATED).build();
 		}
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		return ResponseEntity.badRequest().build();
 	}
-
+	
+	@GetMapping("/courses/enroll/report")
+	ResponseEntity<List<UserReportResponse>> newEnrollment() {
+		List<UserReportResponse> usersReports = enrollmentRepository.countTotalEnrollmentsByUser();
+		if (!usersReports.isEmpty()) {
+			return ResponseEntity.ok(usersReports);
+		}
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/test/java/br/com/alura/school/enrollment/EnrollmentControllerTest.java
+++ src/test/java/br/com/alura/school/enrollment/EnrollmentControllerTest.java
@@ -1,8 +1,6 @@
 package br.com.alura.school.enrollment;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,7 +9,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,7 +38,7 @@ class EnrollmentControllerTest {
     }
     
     @Test
-    void should_validate_bad_enroll_request() throws Exception {
+    void should_validate_bad_enroll_request_course_code_not_found() throws Exception {
     	NewEnrollmentRequest newEnrollmentRequest = new NewEnrollmentRequest("alex");
     	
     	mockMvc.perform(post("/courses/java-555/enroll")
@@ -47,7 +48,7 @@ class EnrollmentControllerTest {
     }
     
     @Test
-    void should_validate_bad_enroll_request_not_found() throws Exception {
+    void should_validate_bad_enroll_request_user_not_found() throws Exception {
     	NewEnrollmentRequest newEnrollmentRequest = new NewEnrollmentRequest("paulo");
     	
     	mockMvc.perform(post("/courses/java-1/enroll")
@@ -66,5 +67,29 @@ class EnrollmentControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(jsonMapper.writeValueAsString(newEnrollmentRequest)))
 				.andExpect(status().isBadRequest());
+    }
+    
+    @Test
+    void should_list_report_from_users_enrollment() throws Exception {
+    	enrollmentRepository.save(new Enrollment("alex", "java-1"));
+    	enrollmentRepository.save(new Enrollment("alex", "java-2"));
+    	enrollmentRepository.save(new Enrollment("ana", "java-1"));
+    	
+    	mockMvc.perform(get("/courses/enroll/report")
+    			.accept(MediaType.APPLICATION_JSON))
+        		.andExpect(status().isOk())
+        		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+        		.andExpect(jsonPath("$.length()", is(2)))
+        		.andExpect(jsonPath("$[0].email", is("alex@email.com")))
+        		.andExpect(jsonPath("$[0].quantidade_matriculas", is(2)))
+        		.andExpect(jsonPath("$[1].email", is("ana@email.com")))
+        		.andExpect(jsonPath("$[1].quantidade_matriculas", is(1)));
+    }
+    
+    @Test
+    void no_content_when_users_reports_is_empty() throws Exception{
+    	 mockMvc.perform(get("/courses/enroll/report")
+                 .accept(MediaType.APPLICATION_JSON))
+                 .andExpect(status().isNoContent());
     }
 }
